7e32ba152b618dc57067f502893cf2c40d9af455
Ryan Gaines <ryan.gaines@uky.edu>
2018-12-07 02:48:39 -0500

    Update works_under.php

--- works_under.php
+++ works_under.php
@@ -30,7 +30,7 @@ function add(){
     echo "<title>set Doctor for Nurse</title>";
     $dName = $_GET['dName'];
     $nName = $_GET['nName'];
-    $link = mysqli_connect("localhost","root", "Password1","hospital");
+    $link = mysqli_connect("localhost","root", "MS-06ZakuII","hospital");
    
     if(!$link){
         die("Failed to connect: " . mysqli_connect_error());
